417eae245a2e1f5959a23778ddf29a39f37770d8
Alex Scarborough <alex@gameclay.com>
2013-12-19 16:23:14 -0800

    Actually use the configurable backend

--- lib/sidekiq/client.rb
+++ lib/sidekiq/client.rb
@@ -188,30 +188,8 @@ module Sidekiq
     private
 
     def raw_push(payloads)
-      @redis_pool.with do |conn|
-        conn.multi do
-          atomic_push(conn, payloads)
-        end
-      end
+      backend.raw_push(payloads)
       true
-    end
-
-    def atomic_push(conn, payloads)
-      if payloads.first['at']
-        conn.zadd('schedule'.freeze, payloads.map do |hash|
-          at = hash.delete('at'.freeze).to_s
-          [at, Sidekiq.dump_json(hash)]
-        end)
-      else
-        q = payloads.first['queue']
-        now = Time.now.to_f
-        to_push = payloads.map do |entry|
-          entry['enqueued_at'.freeze] = now
-          Sidekiq.dump_json(entry)
-        end
-        conn.sadd('queues'.freeze, q)
-        conn.lpush("queue:#{q}", to_push)
-      end
     end
 
     def process_single(worker_class, item)
